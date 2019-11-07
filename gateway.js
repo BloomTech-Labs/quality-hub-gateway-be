e8554a32ec1d531c9325a4d807e3de510fc79bfd
Jonathan Ho <johnnyho82@gmail.com>
2019-11-07 17:04:24 -0500

    Update README.md (not complete)

--- gateway.js
+++ gateway.js
@@ -4,26 +4,29 @@ const { ApolloGateway } = require('@apollo/gateway');
 const gateway = new ApolloGateway({
   serviceList: [
     {
-      name: "core",
-      url: "https://qh-core-practice-01.herokuapp.com"
+      name: 'core',
+      url: 'https://qh-core-practice-01.herokuapp.com',
     },
     {
-        name: 'interviewQ',
-        url: "https://qh-interviewq-practice-01.herokuapp.com",
+      name: 'interviewQ',
+      url: 'https://qh-interviewq-practice-01.herokuapp.com',
     },
     {
-        name: 'resumeQ',
-        url: "https://qh-resumeq-practice-01.herokuapp.com"
+      name: 'resumeQ',
+      url: 'https://qh-resumeq-practice-01.herokuapp.com',
     },
-  ]
-})
-  
-(async () => {
+  ],
+})(async () => {
   const { schema, executor } = await gateway.load();
 
-const server = new ApolloServer({
-    schema, executor, introspection: true, playground: true
- })
+  const server = new ApolloServer({
+    schema,
+    executor,
+    introspection: true,
+    playground: true,
+  });
   const PORT = process.env.PORT || 4000;
-  server.listen(PORT, () => {console.log(`server is listening on ${PORT}`)})
+  server.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`);
+  });
 })();
